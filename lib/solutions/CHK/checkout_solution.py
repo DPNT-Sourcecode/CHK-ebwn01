7fa78b94896d48dac213aabcca4a228664b68b8e
sbx_user1051 <sbx_user1051@169.254.61.13>
2024-03-29 12:17:33 +0000

    Fri Mar 29 12:17:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,26 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    # need to establish a dict for the prices:
+    prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+
+    offers = {
+        'A': (3,130),
+        'B': (2,45)
+    }
+
+    # iterate through input string and count number of each unique sku
+    counts = {}
+    for x in set(skus):
+        counts[x] = skus.count(x)
+    print(counts)
+    
+checkout("ABCABB")
+
