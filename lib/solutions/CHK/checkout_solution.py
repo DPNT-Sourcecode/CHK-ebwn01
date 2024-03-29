e6842d074f5716a6f14ae13498c4061fc55c0012
sbx_user1051 <sbx_user1051@169.254.61.13>
2024-03-29 13:16:07 +0000

    Fri Mar 29 13:16:07 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,51 +1,49 @@
-from collections import Counter
 
+# noinspection PyUnusedLocal
+# skus = unicode string
 def checkout(skus):
+
+    # need to establish a dict for the prices:
     prices = {
         'A': 50,
         'B': 30,
         'C': 20,
-        'D': 15,
-        'E': 40
+        'D': 15
     }
 
     offers = {
-        'A': [(5, 200), (3, 130)],
-        'B': [(2, 45)],
+        'A': (3,130),
+        'B': (2,45),
         'C': None,
-        'D': None,
-        'E': [(2, 'B')]
+        'D': None
     }
 
-    counts = Counter(skus)
-
+    # iterate through input string and count number of each unique sku
+    counts = {}
+    for x in set(skus):
+        counts[x] = skus.count(x)
+    
     total = 0
-    for item, count in counts.items():
-        if item not in prices:
+    # input validation:
+    for x, count in counts.items():
+        if x not in prices:
             return -1
         
-        price = prices[item]
-        offer = offers[item]
+        # get price
+        price = prices[x]
+        # get special offer if any
+        offer = offers[x]
 
         if offer:
-            for offer_info in offer:
-                if isinstance(offer_info[0], int): 
-                    quantity_needed, reduced_price = offer_info
-                    discounted_sets = count // quantity_needed
-                    remaining_items = count % quantity_needed
-                    total += discounted_sets * reduced_price
-                    count = remaining_items 
-                else:  
-                    required_quantity, free_item = offer_info
-                    if free_item in counts and counts[free_item] >= count // required_quantity:
-                        free_items_to_deduct = count // required_quantity
-                        counts[free_item] -= free_items_to_deduct
-                        count -= free_items_to_deduct * required_quantity
-            total += count * prices[item]  
+            quantity_needed, reduced_price = offer
+            discounted_amount = (count // quantity_needed) * reduced_price
+            reminder_items = count % quantity_needed
+            regular_price = reminder_items * price
+            total += discounted_amount + regular_price
+        # if we dont have offer
         else:
             total += count * price
-
     return total
 
-print(checkout("EEB"))
+
 
