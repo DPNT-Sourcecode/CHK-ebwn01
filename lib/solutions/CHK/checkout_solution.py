ccd1de8c63cb0ac23d54a458794c707c4c1ad7ac
sbx_user1051 <sbx_user1051@169.254.61.13>
2024-03-29 12:26:33 +0000

    Fri Mar 29 12:26:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,14 +13,39 @@ def checkout(skus):
 
     offers = {
         'A': (3,130),
-        'B': (2,45)
+        'B': (2,45),
+        'C': None,
+        'D': None
     }
 
     # iterate through input string and count number of each unique sku
     counts = {}
     for x in set(skus):
         counts[x] = skus.count(x)
-    print(counts)
     
-checkout("ABCABB")
+    total = 0
+    # input validation:
+    for x, count in counts:
+        if x not in prices:
+            return -1
+        
+        # get price
+        price = prices[x]
+        # get special offer if any
+        offer = offers[x]
+
+        if offer:
+            quantity_needed, reduced_price = offer
+            discounted_amount = (count // quantity_needed) * reduced_price
+            reminder_items = count % quantity_needed
+            regular_price = reminder_items * price
+            total = discounted_amount + regular_price
+        # if we dont have offer
+        else:
+            total = count * price
+    return total
+
+
+
+
 
