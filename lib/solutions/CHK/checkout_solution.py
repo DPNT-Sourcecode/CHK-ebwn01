ca6d3bf261bd92dbad773dfb5ebb13ecbd0cb5d5
sbx_user1051 <sbx_user1051@169.254.61.13>
2024-03-29 13:13:07 +0000

    Fri Mar 29 13:13:07 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,6 @@
+from collections import Counter
 
-# noinspection PyUnusedLocal
-# skus = unicode string
 def checkout(skus):
-
-    # need to establish a dict for the prices:
     prices = {
         'A': 50,
         'B': 30,
@@ -13,47 +10,42 @@ def checkout(skus):
     }
 
     offers = {
-        'A': [(5,200), (1,130)],
-        'B': [(2,45)],
+        'A': [(5, 200), (3, 130)],
+        'B': [(2, 45)],
         'C': None,
         'D': None,
-        'E': [(2,'B')]
+        'E': [(2, 'B')]
     }
 
-    # iterate through input string and count number of each unique sku
-    counts = {}
-    for x in set(skus):
-        counts[x] = skus.count(x)
-    
+    counts = Counter(skus)
+
     total = 0
-    # input validation:
-    for x, count in counts.items():
-        if x not in prices:
+    for item, count in counts.items():
+        if item not in prices:
             return -1
         
-        # get price
-        price = prices[x]
-        # get special offer if any
-        offer = offers[x]
+        price = prices[item]
+        offer = offers[item]
 
         if offer:
-            for various_offer in offer:
-                if isinstance(various_offer[1], int):  # noqa: E721
-                    quantity_needed, reduced_price = various_offer
-                    discounted_amount = (count // quantity_needed) * reduced_price
-                    reminder_items = count % quantity_needed
-                    regular_price = reminder_items * price
-                    count = reminder_items
-                    total += discounted_amount + regular_price
-                else: 
-                    required_amount, free_item = various_offer
-                    if free_item in counts and counts[free_item] >= count // required_amount:
-                        to_deduct = count // required_amount
-                        counts[free_item] -= to_deduct
-                        count -= to_deduct * required_amount
-            total += count * prices[x]
+            for offer_info in offer:
+                if isinstance(offer_info[0], int): 
+                    quantity_needed, reduced_price = offer_info
+                    discounted_sets = count // quantity_needed
+                    remaining_items = count % quantity_needed
+                    total += discounted_sets * reduced_price
+                    count = remaining_items 
+                else:  
+                    required_quantity, free_item = offer_info
+                    if free_item in counts and counts[free_item] >= count // required_quantity:
+                        free_items_to_deduct = count // required_quantity
+                        counts[free_item] -= free_items_to_deduct
+                        count -= free_items_to_deduct * required_quantity
+            total += count * prices[item]  
         else:
             total += count * price
+
     return total
 
-print(checkout("ABCDABCD"))
+print(checkout("EEB"))
+
