3f1b6bbb53a040d395296cc3eed563427bc72185
sbx_user1051 <sbx_user1051@169.254.75.165>
2024-04-09 14:00:24 +0000

    Tue Apr 09 14:00:24 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -67,7 +67,8 @@ def checkout(skus):
                 total_price += offer_count * offer_price
                 for prod in offered_items:
                     if prod in product_counts:
-                        count -= offer_count
+                        product_counts[prod] -= offer_count
+                count = product_counts[product]
                 print(product_counts)
             else:
                 offer_quantity, offer_item = offer
@@ -113,10 +114,4 @@ print(checkout("XXXYYYZZZZ"))  # Output: 50
 # print(checkout("ABCD"))  # Output: 115
 # print(checkout("AAABB"))  # Output: 175
 # print(checkout("E"))  # Output: -1 (Illegal input)
-
-
-
-
-
-
 
